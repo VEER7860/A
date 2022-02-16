3154b5d078143b26e844e6d4584305e04fa5e884
VEER7860 <86776400+VEER7860@users.noreply.github.com>
2022-02-16 02:08:26 -0800

    Add files via upload

---
+++ Adv-159.py
@@ -0,0 +1,18 @@
+from tkinter import *
+root = Tk()
+root.geometery("600x400")
+
+list_name = ["Mickey mouse", "Elsa", "Anna", "Donald duck"]
+dict_student = {"name" : "Shichan",
+                "age" : "5"}
+try:
+    print(list_name[5])
+    print(dict_student["mom"])
+    
+except IndexError:
+        messagebox.showinfo("Error", "This index does not exist")
+except KeyError:
+        messagebox.showinfo("Error","This key does not exist")
+        
+root.mainloop()        
+        
